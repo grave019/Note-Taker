8136d7540c3672ccc05effecdf9febe29193431c
Brent Graves <graves.brent@gmail.com>
2021-10-20 17:03:02 -0400

    added express and path to send user to index.html.

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//using express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"))
+
+// route that will send the user to the index page
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+
+});
