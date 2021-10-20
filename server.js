393cd458bb9b53f8f77f13dcd4e069eaa7d09088
Brent Graves <graves.brent@gmail.com>
2021-10-20 17:27:52 -0400

    adding paths for d.json and index.html.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const db = require("db.db.json")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -23,4 +24,15 @@ app.get("/", function(req, res) {
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 
+});
+
+// Route that sends file to db/db.json
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "db/db.json"));
+
+});
+//* should return to index.html
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
