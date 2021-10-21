1b76dc975be5c8c1fc7b1a83cace3c1feeaf45b9
Brent Graves <graves.brent@gmail.com>
2021-10-21 16:38:06 -0400

    connected server.js to htmlroutes.js.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,3 +9,5 @@ router.get('/', (req, res) => {
 router.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+const htmlRoutes = require('./routes/htmlRoutes')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
