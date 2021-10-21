7375a56f9172f50c493f09a5f7c212d73dcbe6ae
Brent Graves <graves.brent@gmail.com>
2021-10-21 16:23:08 -0400

    added app.listen for port per start up video.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 //using express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
