522db17a32a858e03d2988949f0985457edfd0cf
Brent Graves <graves.brent@gmail.com>
2021-10-21 16:45:45 -0400

    connected apiRoutes to server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const fs = require("fs");
 
 const htmlRoutes = require('./routes/htmlRoutes')
+const apiRoutes = require('./routes/apiRoutes')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -12,6 +13,8 @@ app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"))
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 // route that will sends user to index.html
 
