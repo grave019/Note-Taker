92257166f252f77b3dc41eb4f8128096eda9657d
Brent Graves <graves.brent@gmail.com>
2021-10-22 12:48:45 -0400

    finally getting notes to save I created a helper folder with fsUtils.js and uuid to help with getting notes to save.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,44 @@
-const path = require('path');
+// Required files + modules
 const router = require('express').Router();
+const { readFromFile, readAndAppend, deleteNote } = require('../helper/fsUtils');
+const uuid = require('../helper/uuid');
 
-router.get('/notes', (req.res) => {
 
+// GET route for retrieving all the notes
+router.get('/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 })
 
-router.post('/notes', (req,res) => {
+// POST route for submitting a new note
+router.post('/notes', (req, res) => {
+    const { title, text } = req.body;
+    // If title and text was entered...
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+        // Read JSON file and append new note
+        readAndAppend(newNote, './db/db.json');
 
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json('Error creating a new note');
+    }
 })
 
+//DELETE request
+router.delete('/notes/:id', (req, res) => {
+    console.log(req.params.id);
+    deleteNote(req.params.id, './db/db.json');
+    res.json('success');
+});
+
+// Export
 module.exports = router;
--- server.js
+++ server.js
@@ -1,77 +1,25 @@
-//dependencies
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-
-const htmlRoutes = require('./routes/htmlRoutes')
-const apiRoutes = require('./routes/apiRoutes')
-const app = express();
+const express = require(`express`);
+const path = require(`path`);
+const api = require(`./routes/apiRoutes`);
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
-//using express to handle data parsing
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"))
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(`/api`, api);
 
-// route that will sends user to index.html
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+app.get(`/`, (req, res) => 
+res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-});
 
-// route that will sends user to notes.html
-
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-
-});
-
-// Route that sends file to db/db.json
-
-app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "db/db.json"));
-
-});
-//* should return to index.html
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a note`);
-
-    let newNote = req.body; 
-    // generates a list of notes and saves it to a var by reading current db
-    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); 
-
-    //creates unique ids using uuid, woo
-    newNote.id = uuid();
-    
-    //pushes our new note onto that array read from the db
-    noteList.push(newNote); 
-
-    //write the updated data to db.json
-    fs.writeFileSync(".db/db.json", JSON.stringify(noteList)); 
-    res.json(noteList);
-
+app.get(`/notes`, (req, res) => {
+res.sendFile(path.join(__dirname, '/public/notes.html'));
 })
 
-app.delete("/api/notes/:id", (req, res) => {
-    console.info(`${req.method} request received.`);
 
-    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let noteId = (req.params.id).toString();
-
-
-    noteList = noteList.filter(selected =>{
-        return selected.id != noteId;
-    })
-
-    //saves the new db without the selected note.
-    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
-    res.json(noteList);
-});
+app.listen(PORT, () =>
+    console.log(`listening on port ${PORT}`)    
+);
